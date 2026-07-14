eeaf86462e2cb71d97912d10de135baadf2cc096
new santosh <solankesantosh7219@gmail.com>
2026-07-14 15:41:09 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,32 @@
+
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+//coonecting with mongodb
+
+main()  //main fun call for db conected or not
+.then(() =>{
+    console.log("connected to DB");
+})
+.catch((err) =>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) =>({
+        ...obj,
+        owner: "6a49056bc1506b4893d0cff3",
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
+
+initDB();
