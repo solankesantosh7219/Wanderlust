bec1d397f242397509c094ad761ddf11e29cf7f8
new santosh <solankesantosh7219@gmail.com>
2026-07-15 13:28:53 +0530

    fix cloudinary configuration

--- cloudConfig.js
+++ cloudConfig.js
@@ -4,7 +4,11 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 console.log("Cloud Name:", process.env.CLOUD_NAME);
 console.log("API Key:", process.env.CLOUD_API_KEY);
-console.log("Secret Length:", process.env.CLOUD_API_SECRET?.length);
+console.log(
+    "Secret Check:",
+    process.env.CLOUD_API_SECRET?.substring(0,3),
+    process.env.CLOUD_API_SECRET?.slice(-3)
+);
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
